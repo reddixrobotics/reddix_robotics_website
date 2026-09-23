b07b8df5c1566e4ca004d642a69517d3ae8e3a9f
satwika <saisatwikadhupam@gmail.com>
2026-09-24 00:18:56 +0530

    fix: force IPv4 for email

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,6 @@
 require('dotenv').config();
+import * as dns from 'dns';
+dns.setDefaultResultOrder('ipv4first');
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
